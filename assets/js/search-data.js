61a341fc79ecb16ffd03e79269ffb73bf20f58a1
tehraninasab <50144546+tehraninasab@users.noreply.github.com>
2025-08-15 20:25:22 +0000

    Deploying to gh-pages from @ tehraninasab/tehraninasab.github.io@4bacf25ee5beeb6173d515c3ff82e0aa15b99216 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,23 +41,7 @@ ninja.data = [{
             window.location.href = "/blog/2025/fine-tuning-foundation-models-for-medical-image-analysis/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
